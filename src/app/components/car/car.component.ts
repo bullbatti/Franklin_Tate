9121cce202a813b59f03d816decc4d4c839ba296
bullbatti <battistaandreaa@gmail.com>
2024-08-22 18:17:37 +0200

    modify video timeout

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -18,9 +18,8 @@ export class CarComponent {
   showDialog() {
     this.visible = true;
   
-    // Dopo 4 secondi, imposta visible a false
     setTimeout(() => {
       this.visible = false;
-    }, 3500); // 4000 millisecondi = 4 secondi
+    }, 4500);
   }
 }
